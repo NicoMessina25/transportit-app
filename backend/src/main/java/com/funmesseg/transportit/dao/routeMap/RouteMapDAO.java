118eeeda2d2fe1c5482addc9fdaa5d42fde71750
Neoclak5 <maximosegovia516@gmail.com>
2023-09-26 19:33:22 -0300

    se arreglo un error de tipeo

--- backend/src/main/java/com/funmesseg/transportit/dao/routeMap/RouteMapDAO.java
+++ backend/src/main/java/com/funmesseg/transportit/dao/routeMap/RouteMapDAO.java
@@ -48,7 +48,7 @@ public class RouteMapDAO {
         Trailer trailer = entityManager.getReference(Trailer.class, routeMapRequest.trailer().trailerId());
 
         for(PackageRequest _package : routeMapRequest.packages()){
-            Package p = entityManager.getReference(Package.class, _package.packageId());
+            Package p = entityManager.getReference(Package.class, _package.packageID());
             routeMap.getPackages().add(p);
         }
 
